a9e5d81e08741737de428fc24c83c8d57ace72d7
Andrew Rusli <andreworusli@gmail.com>
2021-01-06 11:46:15 -0500

    create working app.js server, move index.html and style.css to public folder for serving

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || '3000';
+
+app.use(express.static('public'));
+app.use(express.static('dist'));
+
+app.get('/', (req, res) => {
+//   res.send('hello');
+    res.sendFile('index.html');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
