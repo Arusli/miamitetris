11eeee17dd659d56477de53f9df5502be767dabc
Andrew Rusli <andreworusli@gmail.com>
2021-01-06 18:39:05 -0500

    add images folder to app.js express, update css pathing for palmtrees.jpg

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || '3000';
 
 app.use(express.static('public'));
 app.use(express.static('dist'));
+app.use(express.static('images'));
 
 app.get('/', (req, res) => {
 //   res.send('hello');
